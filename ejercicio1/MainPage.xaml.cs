3f6cdf9c7784d9ab107b022a41d253d84a4007cf
pacheco55 <pachecoyeah55@outlok.com>
2025-02-09 18:08:30 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
+
+            //saveButton.Clicked += OnSaveButtonClicked;
+
+            //deleteButton.Clicked += OnDeleteButtonClicked;
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+
+    }
+}
+
+
+       
